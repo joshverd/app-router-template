0fbd7074e0d84ee085bfe61faad9bfd255b558a9
Joshua Verdehem <joshverd2@gmail.com>
2024-09-29 12:33:48 -0500

    Init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import style from './page.module.scss';
+
+const Home = () => {
+  return (
+    <main className={style.main}>
+      <h1>Hello World</h1>
+    </main>
+  );
+};
+
+export default Home;
